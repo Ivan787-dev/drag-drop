7c537065d3b346103bd0c3269b0925157758de53
Ivan787-dev <85332880+Ivan787-dev@users.noreply.github.com>
2021-06-19 09:50:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 var ball = document.querySelector('.block')
 
-ball.onmousedown = function(e) {
+ball.onmousedown = function (e) {
 
-  var coords = getCoords(ball);
-  var shiftX = e.pageX - coords.left;
-  var shiftY = e.pageY - coords.top;
+    var coords = getCoords(ball);
+    var shiftX = e.pageX - coords.left;
+    var shiftY = e.pageY - coords.top;
 
-  ball.style.position = 'absolute';
-  document.body.appendChild(ball);
-  moveAt(e);
-
-  ball.style.zIndex = 1000; // над другими элементами
-
-  function moveAt(e) {
-    ball.style.left = e.pageX - shiftX + 'px';
-    ball.style.top = e.pageY - shiftY + 'px';
-  }
-
-  document.onmousemove = function(e) {
+    ball.style.position = 'absolute';
+    document.body.appendChild(ball);
     moveAt(e);
-  };
 
-  ball.onmouseup = function() {
-    document.onmousemove = null;
-    ball.onmouseup = null;
-  };
+    ball.style.zIndex = 1000; // над другими элементами
+
+    function moveAt(e) {
+        ball.style.left = e.pageX - shiftX + 'px';
+        ball.style.top = e.pageY - shiftY + 'px';
+    }
+
+    document.onmousemove = function (e) {
+        moveAt(e);
+    };
+    document.ontouchmove = function (e) {
+        moveAt(e);
+    };
+    
+    ball.onmouseup = function () {
+        document.onmousemove = null;
+        document.ontouchmove = null;
+        ball.onmouseup = null;
+    };
 
 }
 
-ball.ondragstart = function() {
-  return false;
+ball.ondragstart = function () {
+    return false;
 };
 
 function getCoords(elem) {   // кроме IE8-
-  var box = elem.getBoundingClientRect();
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
+    var box = elem.getBoundingClientRect();
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
 }
